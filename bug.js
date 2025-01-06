45326fc60d69d1de8a2e92b541fc3c11fe96bc23
BugHunter-X <askzenai@gmail.com>
2025-01-07 00:20:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,2 @@
+function foo(a,b){return a+b;}console.log(foo(1, "2")); //Output: 12
+This is because JavaScript performs string concatenation instead of numerical addition when one operand is a string.
